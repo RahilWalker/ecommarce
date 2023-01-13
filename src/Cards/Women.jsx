bcc685855443ea30ff04089fb393fd8379d7c957
RahilWalker <elitekanishkraj@gmail.com>
2023-01-14 00:33:25 +0530

    womwn

--- src/Cards/Women.jsx
+++ src/Cards/Women.jsx
@@ -19,43 +19,53 @@ export default function Women() {
         <div className="text-xl font-extrabold mt-1">Women</div>
         <div className="text-xl font-extrabold">
           <div className="flex">
-            <button
-              className="  px-3 border roundem-sm py-1 opacity-40"
-              onClick={slideLeft}
-            >
-              {leftIcon}
+            <button className="  px-3 border roundem-sm py-1 opacity-40">
+              Best Sellers
             </button>
-            <button
-              className="  px-3 border roundem-sm ml-3 opacity-40"
-              onClick={slideRight}
-            >
-              {rightIcon}
+            <button className="  px-3 border roundem-sm ml-3 opacity-40">
+              Best Sellers
             </button>
           </div>
         </div>
       </div>
-      <div
-        id="devil"
-        className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
-      >
-        {deals.map((item, id) => (
-          <div
-            key={id}
-            className=" inline-block whitespace-normal p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 "
-          >
-            <div className="rounded-lg bg-white shadow-lg text-center">
-              <div>
-                <img
-                  className=" w-full lg:h-88  object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                  src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
-                  alt=""
-                />
-                <div>Heart start</div>
-                <div>procer</div>
+      <div className="relative flex items-center">
+        <div
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideLeft}
+          size={40}
+        >
+          Left{" "}
+        </div>
+        <div
+          id="devil"
+          className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+        >
+          {deals.map((item, id) => (
+            <div
+              key={id}
+              className=" inline-block whitespace-normal p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 "
+            >
+              <div className="rounded-lg bg-white shadow-lg text-center">
+                <div>
+                  <img
+                    className=" w-full lg:h-88  object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
+                    alt=""
+                  />
+                  <div>Heart start</div>
+                  <div>procer</div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideRight}
+          size={40}
+        >
+          RIght{" "}
+        </div>
       </div>
     </>
   );
